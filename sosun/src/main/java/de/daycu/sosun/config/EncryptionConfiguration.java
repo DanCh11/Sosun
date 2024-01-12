73d6bef3a77da2837473d3e26bfaf8c6dd53671c
DanCh11 <dan.chiriac1453@gmail.com>
2024-01-12 16:15:26 +0100

    added bean name

--- sosun/src/main/java/de/daycu/sosun/config/EncryptionConfiguration.java
+++ sosun/src/main/java/de/daycu/sosun/config/EncryptionConfiguration.java
@@ -1,8 +1,10 @@
 package de.daycu.sosun.config;
 
+import de.daycu.sosun.services.EncryptionService;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,7 +17,7 @@ public class EncryptionConfiguration {
     @Value("${jasypt.encryptor.algorithm}")
     private String encryptionAlgorithm;
 
-    @Bean
+    @Bean("stringEncryptor")
     public StringEncryptor stringEncryptor() {
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setPassword(encryptionPassword);
