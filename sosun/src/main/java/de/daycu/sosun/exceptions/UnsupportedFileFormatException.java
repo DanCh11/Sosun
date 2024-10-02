754acb70913d219d22f2888626049f82b89bbf61
DanCh11 <dan.chiriac1453@gmail.com>
2024-10-02 21:16:21 +0200

    deleted redundant project

--- sosun/src/main/java/de/daycu/sosun/exceptions/UnsupportedFileFormatException.java
+++
@@ -1,7 +0,0 @@
-package de.daycu.sosun.exceptions;
-
-public class UnsupportedFileFormatException extends Exception{
-    public UnsupportedFileFormatException(String message) {
-        super(message);
-    }
-}
--- sosun/src/main/java/de/daycu/sosun/repositories/PhoneNumberRepository.java
+++
@@ -1,7 +0,0 @@
-package de.daycu.sosun.repositories;
-
-import de.daycu.sosun.models.ContactPhoneNumber;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhoneNumberRepository extends CrudRepository<ContactPhoneNumber, Long> { }
