6318a7c6194c092b142b116109439681daef73c7
DanCh11 <dan.chiriac1453@gmail.com>
2024-11-01 18:11:35 +0100

    implemented test for file reading service

--- backend/file-reader/src/test/java/de/service/filereader/FileReaderTest.java
+++ backend/file-reader/src/test/java/de/service/filereader/FileReaderTest.java
@@ -2,101 +2,108 @@ package de.service.filereader;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.io.File;
+import de.service.filereader.file_strategy.CSVReader;
+import de.service.filereader.file_strategy.JSONReader;
+import de.service.filereader.file_strategy.XMLReader;
+import de.service.filereader.file_strategy.YMLReader;
+
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import static de.service.filereader.TestFixtures.*;
+
 @ExtendWith({ SpringExtension.class, MockitoExtension.class })
 class FileReaderTest {
-  private static final String CSV_PATH = "src/test/resources/mockPhoneNumbersList.csv";
-  private static final String JSON_PATH = "src/test/resources/mockPhoneNumbersList.json";
-  private static final String XML_PATH = "src/test/resources/mockPhoneNumbersList.xml";
-  private static final String YML_PATH = "src/test/resources/mockPhoneNumbersList.yml";
 
-  public static final Contact bobsContact = Contact.builder()
-    .id(1L)
-    .phoneNumber("+49123123122")
-    .build();
-
-  public static final Contact billysContact = Contact.builder()
-    .id(2L)
-    .phoneNumber("+491237631321")
-    .build();
-
-  public static final List<Contact> bobsAndBillysContacts = Arrays.asList(
-    bobsContact,
-    billysContact);
-
-  @MockBean
+  @Mock
   private CSVReader csvReader;
 
-  @MockBean
+  @Mock
   private JSONReader jsonReader;
 
-  @MockBean
+  @Mock
   private XMLReader xmlReader;
 
-  @MockBean
+  @Mock
   private YMLReader ymlReader;
+
+  @Mock
+  private FileReaderService fileReaderService;
 
   @Test
   void testCSVContactExtraction() throws IOException {
-    File csvFile = new File(CSV_PATH);
-    when(csvReader.read(csvFile)).thenReturn(bobsAndBillysContacts);
-    when(csvReader.detectFileType(csvFile)).thenReturn("csv");
+    when(csvReader.read(CSV_FILE)).thenReturn(BOB_BILLY_CONTACTS);
+    when(csvReader.detectFileType(CSV_FILE)).thenReturn(FileTypeConstants.CSV);
 
-    List<Contact> extractedContacts = this.csvReader.read(csvFile);
-    String fileExtensionOfCSV = this.csvReader.detectFileType(csvFile);
+    List<Contact> extractedContacts = this.csvReader.read(CSV_FILE);
+    String fileExtensionOfCSV = this.csvReader.detectFileType(CSV_FILE);
 
-    assertEquals(extractedContacts, bobsAndBillysContacts);
-    assertEquals(fileExtensionOfCSV, "csv");
+    assertEquals(extractedContacts, BOB_BILLY_CONTACTS);
+    assertEquals(fileExtensionOfCSV, FileTypeConstants.CSV);
   }
 
   @Test
   void testJSONContactExtraction() throws IOException {
-    File jsonFile = new File(JSON_PATH);
-    when(jsonReader.read(jsonFile)).thenReturn(bobsAndBillysContacts);
-    when(jsonReader.detectFileType(jsonFile)).thenReturn("json");
+    when(jsonReader.read(JSON_FILE)).thenReturn(BOB_BILLY_CONTACTS);
+    when(jsonReader.detectFileType(JSON_FILE)).thenReturn(FileTypeConstants.JSON);
 
-    List<Contact> extractedContacts = this.jsonReader.read(jsonFile);
-    String fileExtensionOfJSON = this.jsonReader.detectFileType(jsonFile);
+    List<Contact> extractedContacts = this.jsonReader.read(JSON_FILE);
+    String fileExtensionOfJSON = this.jsonReader.detectFileType(JSON_FILE);
 
-    assertEquals(extractedContacts, bobsAndBillysContacts);
-    assertEquals(fileExtensionOfJSON, "json");
+    assertEquals(extractedContacts, BOB_BILLY_CONTACTS);
+    assertEquals(fileExtensionOfJSON, FileTypeConstants.JSON);
   }
 
   @Test
   void testXMLContactExtraction() throws IOException {
-    File xmlFile = new File(XML_PATH);
-    when(xmlReader.read(xmlFile)).thenReturn(bobsAndBillysContacts);
-    when(xmlReader.detectFileType(xmlFile)).thenReturn("xml");
+    when(xmlReader.read(XML_FILE)).thenReturn(BOB_BILLY_CONTACTS);
+    when(xmlReader.detectFileType(XML_FILE)).thenReturn(FileTypeConstants.XML);
 
-    List<Contact> extractedContacts = this.xmlReader.read(xmlFile);
-    String fileExtensionOfXML = this.xmlReader.detectFileType(xmlFile);
+    List<Contact> extractedContacts = this.xmlReader.read(XML_FILE);
+    String fileExtensionOfXML = this.xmlReader.detectFileType(XML_FILE);
 
-    assertEquals(extractedContacts, bobsAndBillysContacts);
-    assertEquals(fileExtensionOfXML, "xml");
+    assertEquals(extractedContacts, BOB_BILLY_CONTACTS);
+    assertEquals(fileExtensionOfXML, FileTypeConstants.XML);
   }
 
   @Test
   void testYMLContactExtraction() throws IOException {
-    File ymlFile = new File(YML_PATH);
-    when(ymlReader.read(ymlFile)).thenReturn(bobsAndBillysContacts);
-    when(ymlReader.detectFileType(ymlFile)).thenReturn("yml");
+    when(ymlReader.read(YML_FILE)).thenReturn(BOB_BILLY_CONTACTS);
+    when(ymlReader.detectFileType(YML_FILE)).thenReturn(FileTypeConstants.YML);
 
-    List<Contact> extractedContacts = this.ymlReader.read(ymlFile);
-    String fileExtensionOfYML = this.ymlReader.detectFileType(ymlFile);
+    List<Contact> extractedContacts = this.ymlReader.read(YML_FILE);
+    String fileExtensionOfYML = this.ymlReader.detectFileType(YML_FILE);
 
-    assertEquals(extractedContacts, bobsAndBillysContacts);
-    assertEquals(fileExtensionOfYML, "yml");
+    assertEquals(extractedContacts, BOB_BILLY_CONTACTS);
+    assertEquals(fileExtensionOfYML, FileTypeConstants.YML);
   }
+
+  @Test
+  void testFileReaderService() throws IOException {
+    when(fileReaderService.processFile(CSV_FILE)).thenReturn(BOB_BILLY_CONTACTS);
+    when(fileReaderService.processFile(JSON_FILE)).thenReturn(BOB_BILLY_CONTACTS);
+    when(fileReaderService.processFile(XML_FILE)).thenReturn(BOB_BILLY_CONTACTS);
+    when(fileReaderService.processFile(YML_FILE)).thenReturn(BOB_BILLY_CONTACTS);
+
+    List<Contact> extractedContactsFromCSV = this.fileReaderService.processFile(CSV_FILE);
+    List<Contact> extractedContactsFromJSON = this.fileReaderService.processFile(JSON_FILE);
+    List<Contact> extractedContactsFromXML = this.fileReaderService.processFile(XML_FILE);
+    List<Contact> extractedContactsFromYML = this.fileReaderService.processFile(YML_FILE);
+
+    assertEquals(extractedContactsFromCSV, BOB_BILLY_CONTACTS);
+    assertEquals(extractedContactsFromJSON, BOB_BILLY_CONTACTS);
+    assertEquals(extractedContactsFromXML, BOB_BILLY_CONTACTS);
+    assertEquals(extractedContactsFromYML, BOB_BILLY_CONTACTS);
+
+  }
+
+
 
 }
